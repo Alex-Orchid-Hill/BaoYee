aeca0b138999e231ef4b426e5889189642461a70
LiBaoDeng <libdqd_happy2020@163.com>
2024-08-30 14:20:09 +0800

    new file:   .vuepress/config.js 	new file:   .vuepress/public/avatar.png 	new file:   .vuepress/public/favicon.ico 	new file:   .vuepress/public/hero.png 	new file:   .vuepress/public/logo.png 	new file:   README.md 	new file:   "blogs/\345\267\245\344\275\234\350\256\260\345\275\225/2016/121501.md" 	new file:   "blogs/\345\267\245\344\275\234\350\256\260\345\275\225/2017/092101.md" 	new file:   "blogs/\346\235\202\350\260\210/guide.md" 	new file:   "blogs/\350\256\276\350\256\241\346\250\241\345\274\217/\345\215\225\344\276\213\346\250\241\345\274\217/092101.md" 	new file:   "blogs/\350\256\276\350\256\241\346\250\241\345\274\217/\345\267\245\345\216\202\346\250\241\345\274\217/121501.md" 	new file:   docs/theme-reco/README.md 	new file:   docs/theme-reco/api.md 	new file:   docs/theme-reco/plugin.md 	new file:   docs/theme-reco/theme.md 	new file:   package-lock.json 	new file:   package.json

---
+++ .vuepress/config.js
@@ -0,0 +1,107 @@
+/*
+ * @Author: LiBaoDeng libdqd_happy2020@163.com
+ * @Date: 2024-08-27 17:19:02
+ * @LastEditors: LiBaoDeng libdqd_happy2020@163.com
+ * @LastEditTime: 2024-08-30 13:34:47
+ * @FilePath: \BaoYee\.vuepress\config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = {
+  title: "BaoYee文书笔记",
+  description: 'BaoYee专属个人博客',
+  base:'/vuepress/',
+  dest: 'dist',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
+  theme: 'reco',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/', icon: 'reco-home' },
+      { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
+      { text: 'Docs', 
+        icon: 'reco-message',
+        items: [
+          { text: 'vuepress-reco', link: '/docs/theme-reco/' }
+        ]
+      },
+      { text: 'Contact', 
+        icon: 'reco-message',
+        items: [
+          { text: 'Gitee', link: 'https://gitee.com/iamsupercool', icon: 'reco-github' }
+        ]
+      }
+    ],
+    sidebar: {
+      '/docs/theme-reco/': [
+        '',
+        'theme',
+        'plugin',
+        'api'
+      ]
+    },  
+    type: 'blog',
+    // 博客设置
+    blogConfig: {
+      category: {
+        location: 2, // 在导航栏菜单中所占的位置，默认2
+        text: '知识库' // 默认 “分类”
+      },
+      tag: {
+        location: 3, // 在导航栏菜单中所占的位置，默认3
+        text: 'Tag' // 默认 “标签”
+      }
+    },
+    friendLink: [
+      {
+        title: '紫来峰',
+        desc: 'Enjoy when you can, and endure when you must.',
+        email: '767645258@qq.com',
+        link: 'https://gitee.com/iamsupercool'
+      },
+      {
+        title: '代码康德',
+        desc: '博学笃志，守正出奇',
+        link: 'https://vuepress-theme-reco.recoluan.com'
+      },
+    ],
+    logo: '/logo.png',
+    // 搜索设置
+    search: true,
+    searchMaxSuggestions: 10,
+    // 自动形成侧边导航
+    // sidebar: 'auto',
+    // 最后更新时间
+    lastUpdated: 'Last Updated',
+    // 作者
+    author: 'BaoYee',
+    // 作者头像
+    authorAvatar: '/avatar.png',
+    // 备案号
+    record: 'xxxx',
+    // 项目开始时间
+    startYear: '2017'
+    /**
+     * 密钥 (if your blog is private)
+     */
+
+    // keyPage: {
+    //   keys: ['your password'],
+    //   color: '#42b983',
+    //   lineColor: '#42b983'
+    // },
+
+    /**
+     * valine 设置 (if you need valine comment )
+     */
+
+    // valineConfig: {
+    //   appId: '...',// your appId
+    //   appKey: '...', // your appKey
+    // }
+  },
+  markdown: {
+    lineNumbers: true
+  }
+}  
